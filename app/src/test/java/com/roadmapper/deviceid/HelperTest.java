ba8f6fea8099b748edf02a23928060240f3aad68
roadmapper <vindansam@hotmail.com>
2015-07-27 00:18:31 -0400

    Not sure why I deleted these files...

--- app/src/test/java/com/roadmapper/deviceid/HelperTest.java
+++ app/src/test/java/com/roadmapper/deviceid/HelperTest.java
@@ -1,12 +1,13 @@
-package com.vinay.deviceid;
+package com.roadmapper.deviceid;
 
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class HelperTest {
 
-    private Band mBand1, mBand2, mBand3, mBand4;
+    private Band mBand1, mBand2, mBand3, mBand4, mBand5, mBand6;
     private String frequency1 = "1900", frequency2 = "700";
     private String technology1 = Helper.Technology.GSM, technology2 = Helper.Technology.LTE;
     private int band2 = 13;
@@ -15,6 +16,8 @@ public class HelperTest {
     public void setUp() {
         mBand1 = new Band(frequency1, technology1);
         mBand2 = new Band(frequency2, band2, technology2);
+        mBand5 = new Band("1700/2100", 4, "LTE");
+        mBand6 = new Band("1700", 4, "LTE");
     }
 
     @Test
@@ -28,6 +31,16 @@ public class HelperTest {
     public void createLTE() {
         mBand4 = Helper.createUMTS_LTEBand(technology2 + frequency2 + " (B" + band2 + ")", technology2);
         assertEquals(mBand2, mBand4);
+    }
+
+    @Test
+    public void checkBandEquals() {
+        assertEquals(mBand5, mBand6);
+    }
+
+    @Test
+    public void checkBandEquals2() {
+        assertEquals(mBand6, mBand5);
     }
 
 }
